a3df2e3f9d58bf1030aba467ccf8bfbb92f4e637
Leo Jacquet <leojacquet2003@gmail.com>
2024-10-17 17:38:26 +0200

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { fileURLToPath } from 'url';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    }
+});
