d54c6f721d3e6a79d17ff7ac555ddf00514167fc
Leo Jacquet <leojacquet2003@gmail.com>
2024-11-06 09:51:12 +0100

    update base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/projet-ia',
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
